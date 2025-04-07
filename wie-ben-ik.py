425437839334b88c3864770db4d8ac11062c95a1
Dzevad <dzevadzukic@live.nl>
2025-04-07 22:56:22 +0200

    Hoofdstuk 1

---
+++ wie-ben-ik.py
@@ -0,0 +1,14 @@
+    #Wie ben ik?
+
+#schrijf de voor- en achternaam op
+print("Dzevad Zukic")
+
+#schrijf de voor- en achternaam op zonder nieuwe regel steeds
+print("Dzevad", end='')
+print(" ", end='')
+print("Zukic", end='')
+
+#dit is raar, omdat het onder elkaar gaat
+print("Dzevad")
+print(" ")
+print("Zukic")
